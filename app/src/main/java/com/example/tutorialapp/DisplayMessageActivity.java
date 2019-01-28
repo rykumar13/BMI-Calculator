c97533cb6390a34698b3ab108e640f6204916c97
rykumar13 <rykumar13@gmail.com>
2019-01-28 22:23:14 +1300

    Added BMI calculator

--- app/src/main/java/com/example/tutorialapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/tutorialapp/DisplayMessageActivity.java
@@ -1,13 +1,34 @@
 package com.example.tutorialapp;
 
+import android.content.Intent;
+import android.icu.text.DecimalFormat;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        // Get the Intent that started this activity and extract the string and convert into int
+        Intent intent = getIntent();
+        double height = Double.parseDouble(intent.getStringExtra(MainActivity.EXTRA_MESSAGE));
+        double weight = Double.parseDouble(intent.getStringExtra(MainActivity.EXTRA_MESSAGE2));
+        System.out.println("height: " + height);
+        System.out.println("weight: " + weight);
+        double bmi = weight / (height * height);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+
+        DecimalFormat df = new DecimalFormat("#.##");
+        System.out.print(df.format(bmi));
+        textView.setText("Your BMI is " + df.format(bmi));
     }
 }
--- app/src/main/java/com/example/tutorialapp/MainActivity.java
+++ app/src/main/java/com/example/tutorialapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public static final String EXTRA_MESSAGE2 = "com.example.myfirstapp.MESSAGE2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +19,15 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+
+        EditText editText_height = (EditText) findViewById(R.id.editText);
+        String height = editText_height.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, height);
+
+        EditText editText_weight = (EditText) findViewById(R.id.editText2);
+        String weight = editText_weight.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE2, weight);
+
         startActivity(intent);
     }
 }
